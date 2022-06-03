4d1d60c8b61153454bdfb4b27d08a038a3a3093f
nikit <nikit@LAPTOP-6779A6I3>
2022-06-04 01:51:26 +0300

    Добавьте файлы проекта.

---
+++ vadim1/vadim1.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include <clocale>
+#include <math.h> 
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+	int x, y, pi = 3.14;
+	double F
+	cout << "Введите X: ";
+	cin >> x;
+	cout << "Введите Y: " << "\n";
+	cin >> y;
+	F = (0.75 + 3 * exp(pow(y, 2) - 1)) / (sqrt(fabs(y) + 2) - log(fabs(x - y)) + tan(x - pi)) - 2 * pow(cos((x - y) / 2), 2);
+	cout << F;
+}
